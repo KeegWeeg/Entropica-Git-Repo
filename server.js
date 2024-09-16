614cc76ef4f0e4d9545a8acc8dc8882a7cd960a7
Keegan <Keegan4271@gmail.com>
2024-09-16 12:56:03 -0700

    Initial commit with fresh history

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files (HTML, CSS, JS)
+app.use(express.static('public'));
+
+const buttonCountsFile = './buttonCounts.json';
+
+const buttonCounts = {
+    trevin: 0,
+    benjy: 0,
+    cash: 0,
+    keira: 0,
+    bayleigh: 0
+};
+
+const loadButtonCounts = () => {
+    if (fs.existsSync(buttonCountsFile)) {
+        const data = fs.readFileSync(buttonCountsFile);
+        buttonCounts = JSON.parse(data);
+    }
+};
+
+const saveButtonCounts = () => {
+    fs.writeFileSync(buttonCountsFile, JSON.stringify(buttonCounts));
+};
+
+// Handle socket connections
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+      // Send current button counts to the newly connected user
+      socket.emit('initialCounts', buttonCounts);
+
+      // Handle button clicks from the client
+      socket.on('buttonClicked', (buttonId) => {
+          // Increment the count for the clicked button
+          buttonCounts[buttonId]++;
+
+          io.emit('updateCount', { buttonId, count: buttonCounts[buttonId] });
+        });
+    
+    
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Handle other routes (if any)
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
